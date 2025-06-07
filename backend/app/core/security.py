86ba89f24b5163fc2b8c744dcd1821ffcef3744d
zilloone <jsaudia1212@gmail.com>
2025-06-07 01:29:08 +0100

    I addede utilities functions to hash password, verify password and create access token

---
+++ backend/app/core/security.py
@@ -0,0 +1,27 @@
+# app/core/security.py
+from passlib.context import CryptContext
+from datetime import datetime, timezone, timedelta
+from typing import Any
+from dotenv import load_dotenv
+import os
+import jwt
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(subject: str | Any, expires_delta: timedelta):
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
