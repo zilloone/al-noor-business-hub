4cd0a9a29666e281329a753d9403d0dd3e6b26f6
zilloone <jsaudia1212@gmail.com>
2025-06-10 13:19:49 +0100

    added orders.py file

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import  login, users, products
+from app.api.routes import  login, users, products, orders
 
 
 api_router = APIRouter()
@@ -10,4 +10,5 @@ api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(products.router)
 api_router.include_router(login.router)
+api_router.include_router(orders.router)
 
